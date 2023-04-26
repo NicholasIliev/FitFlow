434c1f10acd78581002699479f8fd7ebb124865c
Nicholas Iliev <nicholas.iliev.2003@gmail.com>
2023-04-26 15:06:34 +0100

    Re-organising files.

--- app/index.js
+++ app/index.js
@@ -3,7 +3,8 @@ import { View, ScrollView, SafeAreaView } from "react-native";
 import { Stack, useRouter } from "expo-router"; 
 
 import { COLORS, icons, images, SIZES } from '../constants'
-import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components'
+
 
 const Home = () => {
     const router = useRouter();
